5da19d3b476bc7b10b5a44c54b23a0a205cf438b
NANMEDIGOU yendoumban <yendoumban.nanmedigou@etu.u-paris.fr>
2025-11-24 13:55:51 +0100

    Utils> Adding Position and Direction

---
+++ game/utils/geometry.cpp
@@ -0,0 +1,66 @@
+#include "geometry.hpp"
+
+
+Position :: Position (int x, int y): x {x}, y {y} {}
+
+int Position :: get_x () const {
+    return x;
+}
+
+int Position :: get_y () const {
+    return y;
+}
+
+void Position :: shift (Direction direction) {
+    switch (direction) {
+        case Direction::UP:
+            -- x;
+            break;
+        case Direction::RIGHT:
+            ++ y;
+            break;
+        case Direction::DOWN:
+            ++ x;
+            break;
+        case Direction::LEFT:
+            -- y;
+            break;
+    }
+}
+
+bool operator == (const Position& first, const Position& second) {
+    return first.get_x() == second.get_x() && first.get_y() == second.get_y();
+}
+
+ostream& operator << (ostream& out, const Position& position) {
+    out << "(" << position.get_x() << ", " << position.get_y() << ")";
+    return out;
+}
+
+
+Direction operator ! (Direction direction) {
+    switch (direction) {
+        case Direction::UP:
+            return Direction::DOWN;
+        case Direction::RIGHT:
+            return Direction::LEFT;
+        case Direction::DOWN:
+            return Direction::UP;
+        default:
+            return Direction::RIGHT;
+    }
+}
+
+ostream& operator << (ostream& out, Direction direction) {
+    switch (direction) {
+        case Direction::UP:
+            return out << "UP";
+        case Direction::RIGHT:
+            return out << "RIGHT";
+        case Direction::DOWN:
+            return out << "DOWN";
+        case Direction::LEFT:
+            return out << "LEFT";
+    }
+    return out;
+}
---
+++ game/utils/geometry.hpp
@@ -0,0 +1,39 @@
+#ifndef _GEOMETRY
+#define _GEOMETRY
+
+#include <iostream>
+
+using namespace std;
+
+
+enum class Direction {
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+};
+
+Direction operator ! (Direction direction);
+
+ostream& operator << (ostream& out, Direction direction);
+
+
+class Position {
+
+    private:
+        int x, y;
+    
+    public:
+        Position (int x, int y);
+        int get_x () const;
+        int get_y () const;
+        void shift (Direction direction);
+
+};
+
+bool operator == (const Position& first, const Position& second);
+
+ostream& operator << (ostream& out, const Position& position);
+
+
+#endif
