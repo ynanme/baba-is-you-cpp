2c904e8696ae03cb8f3354c5be4ab8c25867cf28
NANMEDIGOU yendoumban <yendoumban.nanmedigou@etu.u-paris.fr>
2025-11-24 23:48:10 +0100

    Game> Adding actions

---
+++ game/model/core/action.cpp
@@ -0,0 +1,29 @@
+#include "action.hpp"
+
+
+Action :: Action (Character & initiator, Direction direction, CollisionResult result):
+direction {direction}, result {result} {
+    involved_characters.push_back(initiator);
+}
+
+
+Direction Action :: get_direction () const {
+    return direction;
+}
+
+CollisionResult Action :: get_result () const {
+    return result;
+}
+
+
+Character & Action :: get_initiator () const {
+    return involved_characters.at(0);
+}
+
+vector<Character &> Action :: get_involved_characters () const {
+    return involved_characters;
+}
+
+void Action :: add_involved_character (Character & involved_character) {
+    involved_characters.push_back(involved_character);
+}
---
+++ game/model/core/action.hpp
@@ -0,0 +1,32 @@
+#ifndef _ACTION
+#define _ACTION
+
+#include <vector>
+
+#include "../characters/character.hpp"
+#include "../collision_handlers/collision_handlers.hpp"
+#include "../../utils/geometry.hpp"
+
+
+class Action {
+
+    private:
+        vector<Character &> involved_characters;
+        Direction direction;
+        CollisionResult result;
+
+    public:
+
+        Action (Character & initiator, Direction direction, CollisionResult result);
+
+        Direction get_direction () const;
+        CollisionResult get_result () const;
+        
+        Character & get_initiator () const;
+        vector<Character &> get_involved_characters () const;
+        void add_involved_character (Character & involved_character);
+
+};
+
+
+#endif
